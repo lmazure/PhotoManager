b2a98e5143984e24fff54a196bdc7ca17afd69fa
lmazure <lmazure@386d4766-1365-6f4e-aa78-025d1aaf1182>
2010-02-23 07:32:03 +0000

    clean up of cut 'n paste actions
    git-svn-id: file:///cygdrive/m/laurent_svn_repository/photo%20manager/trunk@7 386d4766-1365-6f4e-aa78-025d1aaf1182

--- lmzr/photomngr/ui/GPSDataDisplay.java
+++ lmzr/photomngr/ui/GPSDataDisplay.java
@@ -2,20 +2,42 @@ package lmzr.photomngr.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
+import javax.swing.KeyStroke;
 
 import lmzr.photomngr.data.GPS.GPSDatabase;
+import lmzr.photomngr.ui.action.ActionQuit;
+import lmzr.photomngr.ui.action.ActionSave;
 
 public class GPSDataDisplay extends JFrame {
 	
 	final private GPSTreeTable a_treeTable;
+	final private JMenuBar a_menubar;
 	
 	GPSDataDisplay(final GPSDatabase database) {
 		
 		super();
 		
+        a_menubar = new JMenuBar();
+		setJMenuBar(a_menubar);
+		
+		final JMenu menuFile = new JMenu("File");
+		menuFile.setMnemonic(KeyEvent.VK_F);
+		a_menubar.add(menuFile);
+		ActionSave a_actionSave = new ActionSave("Save", KeyEvent.VK_S, KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK),"Save",null /*filteredList*/); //TODO gérer le dernier argument
+		final JMenuItem itemSave = new JMenuItem(a_actionSave);
+		menuFile.add(itemSave);
+		final ActionQuit actionQuit = new ActionQuit("Quit", KeyEvent.VK_Q, KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK),"Exit",null /*filteredList*/);  //TODO gérer le dernier argument
+		final JMenuItem itemQuit = new JMenuItem(actionQuit);
+		menuFile.add(itemQuit);
+
 		final Container pane = getContentPane();
 		pane.setLayout(new BorderLayout());
 		a_treeTable = new GPSTreeTable(database);
--- lmzr/photomngr/ui/PhotoListDisplay.java
+++ lmzr/photomngr/ui/PhotoListDisplay.java
@@ -2,17 +2,9 @@ package lmzr.photomngr.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.ClipboardOwner;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.IOException;
 
 import javax.swing.Action;
 import javax.swing.JFrame;
@@ -31,6 +23,10 @@ import lmzr.photomngr.data.Photo;
 import lmzr.photomngr.data.PhotoList;
 import lmzr.photomngr.data.PhotoListMetaDataEvent;
 import lmzr.photomngr.data.PhotoListMetaDataListener;
+import lmzr.photomngr.ui.action.ActionCopy;
+import lmzr.photomngr.ui.action.ActionCopyFromNext;
+import lmzr.photomngr.ui.action.ActionCopyFromPrevious;
+import lmzr.photomngr.ui.action.ActionPaste;
 import lmzr.photomngr.ui.action.ActionQuit;
 import lmzr.photomngr.ui.action.ActionSave;
 import lmzr.photomngr.ui.action.PhotoManagerAction;
@@ -53,177 +49,10 @@ public class PhotoListDisplay extends JFrame
 	final private ActionCopyFromNext a_actionCopyFromNext;
 	final private ActionCopyFromPrevious a_actionCopyFromPrevious;
 	
-	/**
-	 * Action to paste
-	 */
-	private class ActionPaste extends PhotoManagerAction {
-	
-		/**
-		 * @param text
-		 * @param mnemonic
-		 * @param accelerator
-		 * @param tooltipText
-		 */
-		public ActionPaste(final String text,
-                           final int mnemonic,
-                           final KeyStroke accelerator,
-                           final String tooltipText) {
-			super(text, mnemonic, accelerator, tooltipText);
-		}
-	
-	
-		/**
-		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-		 */
-		public void actionPerformed(@SuppressWarnings("unused") final ActionEvent e) {
-			
-			final Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);			
-			String text = null;
-			
-			try {
-				if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-					text = (String)t.getTransferData(DataFlavor.stringFlavor);
-				}
-			} catch (final UnsupportedFlavorException e1) {
-				e1.printStackTrace();
-				return;
-			} catch (final IOException e1) {
-				e1.printStackTrace();
-				return;
-			}
-			
-			final int select[] = a_selection.getSelection();
-			if (select.length==0) return;
 
-			if ( a_table.getSelectedColumn() == -1) return;
-			int col = a_table.convertColumnIndexToModel(a_table.getSelectedColumn());
-			
-			for (int i=0; i<select.length; i++) {
-				if ( a_list.isCellEditable(select[i],col)) a_list.setValueAt(text,select[i],col);
-			}
-		}
-	}
-	
-	/**
-	 * Action to copy
-	 */
-	private class ActionCopy extends PhotoManagerAction implements ClipboardOwner {
-	
-		/**
-		 * @param text
-		 * @param mnemonic
-		 * @param accelerator
-		 * @param tooltipText
-		 */
-		public ActionCopy(final String text,
-                          final int mnemonic,
-                          final KeyStroke accelerator,
-                          final String tooltipText) {
-			super(text, mnemonic, accelerator, tooltipText);
-		}
 	
 	
-		/**
-		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-		 */
-		public void actionPerformed(@SuppressWarnings("unused") final ActionEvent e) {
 
-			final int select[] = a_selection.getSelection();
-			if (select.length!=1) return;
-
-			if ( a_table.getSelectedColumn() == -1) return;
-			int col = a_table.convertColumnIndexToModel(a_table.getSelectedColumn());
-
-			final StringSelection fieldContent = new StringSelection(a_list.getValueAt(select[0],col).toString());
-			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(fieldContent, this);
-		}
-		
-		/**
-		 * @see java.awt.datatransfer.ClipboardOwner#lostOwnership(java.awt.datatransfer.Clipboard, java.awt.datatransfer.Transferable)
-		 */
-		public void lostOwnership(@SuppressWarnings("unused") Clipboard clipboard,
-				                  @SuppressWarnings("unused") Transferable contents) {
-		}
-	}
-	
-	/**
-	 * Action to copy a parameter from the previous image
-	 */
-	private class ActionCopyFromPrevious extends PhotoManagerAction {
-	
-		/**
-		 * @param text
-		 * @param mnemonic
-		 * @param accelerator
-		 * @param tooltipText
-		 */
-		public ActionCopyFromPrevious(final String text,
-                                      final int mnemonic,
-                                      final KeyStroke accelerator,
-                                      final String tooltipText) {
-			super(text, mnemonic, accelerator, tooltipText);
-		}
-	
-	
-		/**
-		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-		 */
-		public void actionPerformed(@SuppressWarnings("unused") final ActionEvent e) {
-			
-			final int select[] = a_selection.getSelection();
-			if (select.length==0) return;
-			if (select[0]==0) return;
-
-			if ( a_table.getSelectedColumn() == -1) return;
-			final int col = a_table.convertColumnIndexToModel(a_table.getSelectedColumn());
-			
-            final Object value = a_list.getValueAt(select[0]-1,col);
-			for (int i=0; i<select.length; i++) {
-				if ( a_list.isCellEditable(select[i],col)) a_list.setValueAt(value,select[i],col);
-			}
-
-		}
-	}
-	
-	/**
-	 * Action to copy a parameter from the next image
-	 */
-	private class ActionCopyFromNext extends PhotoManagerAction {
-	
-		/**
-		 * @param text
-		 * @param mnemonic
-		 * @param accelerator
-		 * @param tooltipText
-		 */
-		public ActionCopyFromNext(final String text,
-                                  final int mnemonic,
-                                  final KeyStroke accelerator,
-                                  final String tooltipText) {
-			super(text, mnemonic, accelerator, tooltipText);
-		}
-	
-	
-		/**
-		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-		 */
-		public void actionPerformed(@SuppressWarnings("unused") final ActionEvent e) {
-			
-			final int select[] = a_selection.getSelection();
-			if (select.length==0) return;
-			if (select[select.length-1]==a_list.getRowCount()-1) return;
-
-			if ( a_table.getSelectedColumn() == -1) return;
-			final int col = a_table.convertColumnIndexToModel(a_table.getSelectedColumn());
-			
-			final Object value = a_list.getValueAt(select[select.length-1]+1,col);
-			
-			for (int i=0; i<select.length; i++) {
-				if ( a_list.isCellEditable(select[i],col)) a_list.setValueAt(value,select[i],col);
-			}
-		}
-	}
-	
 
 	/**
 	 * Action to rename a folder
@@ -284,7 +113,7 @@ public class PhotoListDisplay extends JFrame
 		    }
 
 		    // save
-			a_actionSave.actionPerformed(e);
+			a_actionSave.actionPerformed(e); //TODO ce hack est vraiment très sale!!!
 		}
 	}
 	
@@ -298,7 +127,9 @@ public class PhotoListDisplay extends JFrame
 	    super();
 	    
 	    a_list = filteredList;
-	    
+
+		a_table = new PhotoListTable(unfilteredList,filteredList);
+
 	    photoListMetaDataChanged(new PhotoListMetaDataEvent(filteredList,PhotoListMetaDataEvent.PHOTOLIST_IS_SAVED));
         filteredList.addMetaListener(this);
 
@@ -319,16 +150,16 @@ public class PhotoListDisplay extends JFrame
 		final JMenu menuEdit = new JMenu("Edit");
 		menuEdit.setMnemonic(KeyEvent.VK_E);
 		a_menubar.add(menuEdit);
-		a_actionCopy = new ActionCopy("Copy", KeyEvent.VK_C, KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK),"Copy");
+		a_actionCopy = new ActionCopy("Copy", KeyEvent.VK_C, KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK),"Copy",a_table);
 		final JMenuItem itemCopy = new JMenuItem(a_actionCopy);
 		menuEdit.add(itemCopy);
-		a_actionPaste = new ActionPaste("Paste", KeyEvent.VK_V, KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK),"Paste");
+		a_actionPaste = new ActionPaste("Paste", KeyEvent.VK_V, KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK),"Paste",a_table);
 		final JMenuItem itemPaste = new JMenuItem(a_actionPaste);
 		menuEdit.add(itemPaste);
-		a_actionCopyFromPrevious = new ActionCopyFromPrevious("Copy parameter from previous", KeyEvent.CHAR_UNDEFINED, KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK),"Copy the parameter from the previous photo");
+		a_actionCopyFromPrevious = new ActionCopyFromPrevious("Copy parameter from previous", KeyEvent.CHAR_UNDEFINED, KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK),"Copy the parameter from the previous photo",a_table);
 		final JMenuItem itemCopyFromPrevious = new JMenuItem(a_actionCopyFromPrevious);
 		menuEdit.add(itemCopyFromPrevious);
-		a_actionCopyFromNext = new ActionCopyFromNext("Copy parameter from next", KeyEvent.CHAR_UNDEFINED, KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK),"Copy the parameter from the previous photo");
+		a_actionCopyFromNext = new ActionCopyFromNext("Copy parameter from next", KeyEvent.CHAR_UNDEFINED, KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK),"Copy the parameter from the previous photo",a_table);
 		final JMenuItem itemCopyFromNext = new JMenuItem(a_actionCopyFromNext);
 		menuEdit.add(itemCopyFromNext);
 		a_actionRenameFolder = new ActionRenameFolder("Rename folder", KeyEvent.CHAR_UNDEFINED, null,"Rename the folder");
@@ -337,7 +168,6 @@ public class PhotoListDisplay extends JFrame
 
 		final Container pane = getContentPane();
 		pane.setLayout(new BorderLayout());
-		a_table = new PhotoListTable(unfilteredList,filteredList);
 		final JScrollPane scrollPane = new JScrollPane(a_table);
 		pane.add(scrollPane, BorderLayout.CENTER);
 
