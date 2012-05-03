8f49830d55f4ccc48c709f407d647fb9a0063920
Laurent <Laurent@386d4766-1365-6f4e-aa78-025d1aaf1182>
2012-05-03 19:42:41 +0000

    Creation of EnhancedJTable
    git-svn-id: file:///cygdrive/m/laurent_svn_repository/photo%20manager/trunk@114 386d4766-1365-6f4e-aa78-025d1aaf1182

--- lmzr/photomngr/ui/PhotoListTable.java
+++ lmzr/photomngr/ui/PhotoListTable.java
@@ -1,18 +1,11 @@
 package lmzr.photomngr.ui;
 
 import java.awt.AWTEvent;
-import java.awt.Component;
 import java.awt.Frame;
-import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
 
-import javax.swing.JLabel;
-import javax.swing.JTable;
-import javax.swing.JViewport;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
-import javax.swing.table.TableCellRenderer;
 
 import lmzr.photomngr.data.PhotoList;
 import lmzr.photomngr.data.phototrait.PhotoOriginality;
@@ -29,11 +22,12 @@ import lmzr.photomngr.ui.celleditor.ZoomCellEditor;
 import lmzr.photomngr.ui.cellrenderer.DateCellRenderer;
 import lmzr.photomngr.ui.cellrenderer.LocationCellRenderer;
 import lmzr.photomngr.ui.cellrenderer.SubjectCellRenderer;
+import lmzr.util.ui.EnhancedJTable;
 
 /**
  * 
  */
-public class PhotoListTable extends JTable {
+public class PhotoListTable extends EnhancedJTable {
 
     private boolean a_setVisibility;
     
@@ -104,86 +98,5 @@ public class PhotoListTable extends JTable {
         final int row = getSelectionModel().getMinSelectionIndex();
         if ( row == -1 ) return;
         if (!isCellVisible(row,0)) scrollToVisible(row,0);
-    }
-
-    /**
-     * see http://javaalmanac.com/egs/javax.swing.table/IsVis.html
-     * @param rowIndex
-     * @param vColIndex
-     * @return boolean indicating if the cell is visible
-     */
-    private boolean isCellVisible(final int rowIndex,
-                                  final int vColIndex) {
-        final JViewport viewport = (JViewport)getParent();
-        if (viewport == null) {
-            // we are in the creation of the table, it has not been attached to a JScrollPane yet
-            return true;
-        }
-    
-        // This rectangle is relative to the table where the
-        // northwest corner of cell (0,0) is always (0,0)
-        final Rectangle rect = getCellRect(rowIndex, vColIndex, true);
-    
-        // The location of the viewport relative to the table
-        final Point pt = viewport.getViewPosition();
-    
-        // Translate the cell location so that it is relative
-        // to the view, assuming the northwest corner of the
-        // view is (0,0)
-        rect.setLocation(rect.x-pt.x, rect.y-pt.y);
-    
-        // Check if view completely contains cell
-        return new Rectangle(viewport.getExtentSize()).contains(rect);
-    }
-    
-    /**
-     * see http://javaalmanac.com/egs/javax.swing.table/Vis.html
-     * @param rowIndex
-     * @param vColIndex
-     */
-    private void scrollToVisible(final int rowIndex,
-                                 final int vColIndex) {
-    	
-        final JViewport viewport = (JViewport)getParent();
-           
-        // This rectangle is relative to the table where the
-        // northwest corner of cell (0,0) is always (0,0).
-        //final Rectangle rect1 = getVisibleRect();
-        //System.err.println("rect1 = "+rect1.width+"x"+rect1.height+" "+rect1.x+" "+rect1.y);
-        final Rectangle rect = getCellRect(rowIndex, vColIndex, true);
-        //System.err.println("rect = "+rect.width+"x"+rect.height+" "+rect.x+" "+rect.y);
-    
-        // The location of the viewport relative to the table
-        final Point pt = viewport.getViewPosition();
-        //System.err.println("pt = "+pt.x+" "+pt.y);
-    
-        // Translate the cell location so that it is relative
-        // to the view, assuming the northwest corner of the
-        // view is (0,0)
-        rect.setLocation(rect.x-pt.x, rect.y-pt.y);
-    
-        // Scroll the area into view
-        viewport.scrollRectToVisible(rect);
-    }
-
-    /**
-     * This method is overwritten to display a tooltip in order to show the cell's content
-     * when this one is larger than the cell's size
-     * @see javax.swing.JComponent#getToolTipText(java.awt.event.MouseEvent)
-     */
-    @Override
-	public String getToolTipText(final MouseEvent event)
-    {
-        // TODO le tooltip devrait aussi s'afficher quand la dernière colonne est partiellement cachée
-        final int row = rowAtPoint(event.getPoint());
-        final int col = columnAtPoint(event.getPoint());
-        if ( (row == -1) || (col == -1) ) return null;
-
-        final Object obj = getValueAt(row,col);
-        final TableCellRenderer tcr = getDefaultRenderer(getModel().getColumnClass(convertColumnIndexToModel(col)));
-        final Component c = tcr.getTableCellRendererComponent(this,obj,false,false,row,col);
-        if (c.getPreferredSize().width < getCellRect(row, col, true).width) return null;
-        if (c instanceof JLabel) return ((JLabel)c).getText();
-        return obj.toString();
     }
 }
