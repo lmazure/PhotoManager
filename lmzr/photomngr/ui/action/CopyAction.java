ebbb71b2129ac03c5003657c24b0d61018b4ee1d
Laurent <Laurent@386d4766-1365-6f4e-aa78-025d1aaf1182>
2013-10-23 17:46:47 +0000

    Fixed incorrect format when copying a float
    git-svn-id: file:///cygdrive/m/laurent_svn_repository/photo%20manager/trunk@126 386d4766-1365-6f4e-aa78-025d1aaf1182

--- lmzr/photomngr/ui/action/CopyAction.java
+++ lmzr/photomngr/ui/action/CopyAction.java
@@ -6,10 +6,15 @@ import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
+import java.text.NumberFormat;
 
 import javax.swing.JTable;
 import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
+
+import lmzr.photomngr.data.phototrait.PhotoTrait;
+import lmzr.util.string.HierarchicalCompoundString;
+import lmzr.util.string.MultiHierarchicalCompoundString;
 
 /**
  * Action to copy
@@ -50,7 +55,24 @@ public class CopyAction extends PhotoManagerAction implements ClipboardOwner {
 		final int selectedColumn = a_table.getSelectedColumn();
 		if ( selectedColumn == -1) return;
 
-		final StringSelection fieldContent = new StringSelection(a_table.getValueAt(selection.getMinSelectionIndex(),selectedColumn).toString());
+		Object value = a_table.getValueAt(selection.getMinSelectionIndex(),selectedColumn);
+		String str;
+		if (value instanceof String) {
+			str = (String)value;
+		} else if (value instanceof HierarchicalCompoundString) {
+			str = ((HierarchicalCompoundString)value).toString();
+		} else if (value instanceof MultiHierarchicalCompoundString) {
+			str = ((MultiHierarchicalCompoundString)value).toString();
+		} else if (value instanceof PhotoTrait) {
+			str = ((PhotoTrait)value).toString();
+		} else if (value instanceof Integer) {
+			str = NumberFormat.getInstance().format((Integer)value);
+		} else if (value instanceof Float) {
+			str = NumberFormat.getInstance().format((Float)value);
+		} else {
+			throw new IllegalArgumentException("Unsupported type");
+		}
+		final StringSelection fieldContent = new StringSelection(str);
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(fieldContent, this);
 	}
 	
