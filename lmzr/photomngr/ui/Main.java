da8232619d6bcb6b97569e6c7039be61fad80ec3
lma <lma@386d4766-1365-6f4e-aa78-025d1aaf1182>
2017-07-18 20:36:22 +0000

    Added validation of command line parameters.
    git-svn-id: file:///cygdrive/m/laurent_svn_repository/photo%20manager/trunk@131 386d4766-1365-6f4e-aa78-025d1aaf1182

--- lmzr/photomngr/ui/Main.java
+++ lmzr/photomngr/ui/Main.java
@@ -26,6 +26,12 @@ public class Main {
      */
     public static void main(String[] args) {
     	Chrono.setBeginningOfTime();
+    	
+    	if (args.length != 2) {
+    		System.err.println("Syntax: PhotoManager <photoDirectory> <cacheDirectory>");
+    		System.exit(1);
+    	}
+    	
         new Main(args[0],args[1]);
     }
     
